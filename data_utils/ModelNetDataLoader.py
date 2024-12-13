02d94c54f9fa08196f05b2c4e2016b89b6ed28e7
CY637 <surpolotim420@gmail.com>
2024-12-13 17:38:20 +0800

    checkout data format

--- data_utils/ModelNetDataLoader.py
+++ data_utils/ModelNetDataLoader.py
@@ -83,6 +83,7 @@ class ModelNetDataLoader(Dataset):
             self.save_path = os.path.join(root, 'modelnet%d_%s_%dpts.dat' % (self.num_category, split, self.npoints))
 
         if self.process_data:
+            print(os.path)
             if not os.path.exists(self.save_path):
                 print('Processing data %s (only running in the first time)...' % self.save_path)
                 self.list_of_points = [None] * len(self.datapath)
@@ -139,8 +140,28 @@ class ModelNetDataLoader(Dataset):
 
 if __name__ == '__main__':
     import torch
-
-    data = ModelNetDataLoader('/data/modelnet40_normal_resampled/', split='train')
+    import argparse
+    def parse_args():
+        '''PARAMETERS'''
+        parser = argparse.ArgumentParser('training')
+        # parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
+        # parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
+        # parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
+        # parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
+        parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
+        # parser.add_argument('--epoch', default=200, type=int, help='number of epoch in training')
+        # parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
+        parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
+        # parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
+        # parser.add_argument('--log_dir', type=str, default=None, help='experiment root')
+        # parser.add_argument('--decay_rate', type=float, default=1e-4, help='decay rate')
+        parser.add_argument('--use_normals', action='store_true', default=False, help='use normals')
+        parser.add_argument('--process_data', action='store_true', default=False, help='save data offline')
+        parser.add_argument('--use_uniform_sample', action='store_true', default=False, help='use uniform sampiling')
+        return parser.parse_args()
+    args = parse_args()
+    
+    data = ModelNetDataLoader('/data/modelnet40_normal_resampled/', args, split='train', process_data=True)
     DataLoader = torch.utils.data.DataLoader(data, batch_size=12, shuffle=True)
     for point, label in DataLoader:
         print(point.shape)
